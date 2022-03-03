a938e67f2d7b2347250f043db8f09cf2fe533e9f
HoChiTai <hochitai24@gmail.com>
2022-03-03 21:25:22 +0700

    new resource

--- src/index.js
+++ src/index.js
@@ -5,25 +5,28 @@ const handlebars = require('express-handlebars').create( { extname: '.hbs'});
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 //HTTP logger
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 
 //Template engine
 app.engine('hbs', handlebars.engine)
 app.set('view engine', 'hbs');
 app.set("views",path.join(__dirname, 'resources/views'));
 
-console.log(__dirname);
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
